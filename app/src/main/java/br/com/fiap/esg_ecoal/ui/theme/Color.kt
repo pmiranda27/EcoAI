15ba0d712d21ddfa88fbfadec52458b74e38a135
pmiranda27 <pmiranda.senai29@gmail.com>
2026-03-02 16:48:52 -0300

    Feat: Paleta de Cores Light e Theme.kt

--- app/src/main/java/br/com/fiap/esg_ecoal/ui/theme/Color.kt
+++ app/src/main/java/br/com/fiap/esg_ecoal/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package br.com.fiap.esg_ecoal.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Paleta Light Mode
+val LightPrimary = Color(0xFFD35D6E)
+val LightSecondary = Color(0xFF8A588B)
+val LightInversePrimary = Color(0xFF5F5785)
+val LightTertiary = Color(0xFF574143)
+val LightOnPrimary = Color(0xFFFFFFFF)
 
+// Paleta Dark Mode
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
