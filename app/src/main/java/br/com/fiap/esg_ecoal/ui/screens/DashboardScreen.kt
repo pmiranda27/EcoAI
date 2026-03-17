dce189c6c5dc354b2803deda24de62f8c3522ff7
lotouux <beatriz.c.serafini@gmail.com>
2026-03-16 21:24:06 -0300

    Ajustando imports dashboard

--- app/src/main/java/br/com/fiap/esg_ecoal/ui/screens/DashboardScreen.kt
+++ app/src/main/java/br/com/fiap/esg_ecoal/ui/screens/DashboardScreen.kt
@@ -1,13 +1,26 @@
 package br.com.fiap.esg_ecoal.ui.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,13 +33,16 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import br.com.fiap.esg_ecoal.ui.components.*
-import br.com.fiap.esg_ecoal.ui.theme.ESGEcoalTheme
 import br.com.fiap.esg_ecoal.R
 import br.com.fiap.esg_ecoal.data.model.DashboardData
 import br.com.fiap.esg_ecoal.data.model.UiState
 import br.com.fiap.esg_ecoal.factory.ViewModelFactory
 import br.com.fiap.esg_ecoal.repository.AnalyticsRepository
+import br.com.fiap.esg_ecoal.ui.components.AppBarDefaultWithGoBackButton
+import br.com.fiap.esg_ecoal.ui.components.BarChartComponent
+import br.com.fiap.esg_ecoal.ui.components.DoughnutChartComponent
+import br.com.fiap.esg_ecoal.ui.components.GroupedBarChartComponent
+import br.com.fiap.esg_ecoal.ui.theme.ESGEcoalTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
